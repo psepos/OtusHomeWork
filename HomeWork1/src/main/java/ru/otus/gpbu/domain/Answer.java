11d1bf51f501c355a2acb3f19cf4b5a2c1ec1841
palag <palagos@yandex.ru>
2021-07-02 23:32:08 +0300

    wwww

--- HomeWork1/src/main/java/ru/otus/gpbu/domain/Answer.java
+++ HomeWork1/src/main/java/ru/otus/gpbu/domain/Answer.java
@@ -1,4 +1,9 @@
 package ru.otus.gpbu.domain;
 
 public class Answer {
+    public String answer;
+
+    public Answer(String answer) {
+        this.answer = answer;
+    }
 }
--- HomeWork1/src/main/java/ru/otus/gpbu/domain/Question.java
+++ HomeWork1/src/main/java/ru/otus/gpbu/domain/Question.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Question {
     public String Question;
-    public List<Answer> Answers;
+    public List<Answer> Answers = new List<Answer>();
 
+    public
 }
