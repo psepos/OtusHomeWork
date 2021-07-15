891a126a3a505b50b66dc8e856a4ed8716d59cbc
Palagin Sergey <sergey.palagin@gazprombank.ru>
2021-07-16 00:48:14 +0300

    ррро

--- HomeWork01/src/main/java/ru/otus/gpbu/pse/homework01/Main.java
+++ HomeWork01/src/main/java/ru/otus/gpbu/pse/homework01/Main.java
@@ -1,16 +1,14 @@
 package ru.otus.gpbu.pse.homework01;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.otus.gpbu.pse.homework01.controller.MyQuestionController;
-import ru.otus.gpbu.pse.homework01.controller.MyQuestionControllerMain;
-import ru.otus.gpbu.pse.homework01.dao.GetQuestionsFromCsvDao;
-import ru.otus.gpbu.pse.homework01.service.GetQuestionsServiceSimple;
-import ru.otus.gpbu.pse.homework01.ui.MyQuestionsConsoleUI;
 
 public class Main {
-    private static final String CSV_FILE = "BOOT-INF/classes/data-source/questions-list.csv";
 
     public static void main(String[] args) {
-        MyQuestionController controller = new MyQuestionControllerMain(new GetQuestionsServiceSimple(new GetQuestionsFromCsvDao(CSV_FILE)), new MyQuestionsConsoleUI());
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        MyQuestionController controller = context.getBean(MyQuestionController.class);
         controller.run();
     }
 
