acb8ca19c925ef44739dc26cdeb2a0a19c3ebc0c
Palagin Sergey <sergey.palagin@gazprombank.ru>
2021-07-30 11:15:42 +0300

    ррро

--- HomeWork02/src/main/java/ru/otus/gpbu/pse/homework02/config/DaoConfig.java
+++ HomeWork02/src/main/java/ru/otus/gpbu/pse/homework02/config/DaoConfig.java
@@ -3,10 +3,12 @@ package ru.otus.gpbu.pse.homework02.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import ru.otus.gpbu.pse.homework02.dao.GetQuestionsDao;
 import ru.otus.gpbu.pse.homework02.dao.GetQuestionsFromCsvDao;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class DaoConfig {
     @Bean
     GetQuestionsDao getQuestionsDao(@Value("${csv.file}") String csvFile,
