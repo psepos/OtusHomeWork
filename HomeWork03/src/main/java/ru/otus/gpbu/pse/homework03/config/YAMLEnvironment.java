f03b9aaf70ae8c35068435178395217c3842b307
Palagin Sergey <palagos@yandex.ru>
2021-08-15 23:32:00 +0300

    HomeWork03-02

--- HomeWork03/src/main/java/ru/otus/gpbu/pse/homework03/config/YAMLEnvironment.java
+++ HomeWork03/src/main/java/ru/otus/gpbu/pse/homework03/config/YAMLEnvironment.java
@@ -1,6 +1,5 @@
 package ru.otus.gpbu.pse.homework03.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
