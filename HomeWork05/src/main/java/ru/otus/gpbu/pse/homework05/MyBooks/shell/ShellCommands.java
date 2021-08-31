7dde7c1686e0a58c8a4042ab16736e3d93724af0
Palagin Sergey <palagos@yandex.ru>
2021-09-01 00:36:47 +0300

    Homework05

---
+++ HomeWork05/src/main/java/ru/otus/gpbu/pse/homework05/MyBooks/shell/ShellCommands.java
@@ -0,0 +1,15 @@
+package ru.otus.gpbu.pse.homework05.MyBooks.shell;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+
+@ShellComponent
+public class ShellCommands {
+
+    @ShellMethod(value = "Run")
+    public void run(){
+
+    }
+
+}
