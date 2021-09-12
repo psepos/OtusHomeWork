ae08b1ff3a0142bb1c4427100557a1d849110dda
Palagin Sergey <palagos@yandex.ru>
2021-09-12 15:29:21 +0300

    Homework05

--- HomeWork05/src/test/java/ru/otus/gpbu/pse/homework05/test/dao/GenreDaoJdbcTest.java
+++ HomeWork05/src/test/java/ru/otus/gpbu/pse/homework05/test/dao/GenreDaoJdbcTest.java
@@ -5,9 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import ru.otus.gpbu.pse.homework05.Homework05Application;
+import ru.otus.gpbu.pse.homework05.myybooks.dao.DoesNotExistException;
 import ru.otus.gpbu.pse.homework05.myybooks.dao.GenreDao;
 import ru.otus.gpbu.pse.homework05.myybooks.domain.DomainObjectFactory;
 import ru.otus.gpbu.pse.homework05.myybooks.domain.Genre;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +19,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GenreDaoJdbcTest {
     @Autowired
     private GenreDao dao;
+
+    @Test
+    void count() {
+        long count = dao.count();
+        assertEquals(4, count);
+    }
 
     @Test
     void getById() {
@@ -60,4 +69,25 @@ public class GenreDaoJdbcTest {
         assertEquals(genre, updatedGenre);
         assertEquals("Genre22", updatedGenre.name());
     }
+
+    @Test
+    void deleteById() {
+        dao.deleteById(1L);
+        assertThrows(DoesNotExistException.class, () -> dao.getById(1L));
+    }
+
+    @Test
+    void getAll() {
+        List<Genre> all = dao.getAll();
+        assertNotNull(all);
+
+        assertEquals(3, all.size());
+
+        assertEquals("Genre1", all.get(0).name());
+        assertEquals("Genre2", all.get(1).name());
+        assertEquals("Genre3", all.get(2).name());
+
+    }
+
+
 }
