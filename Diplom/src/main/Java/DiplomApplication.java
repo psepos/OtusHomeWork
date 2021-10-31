eb3a380e104559123bbb9039b90af07d0fc20bbf
Palagin Sergey <palagos@yandex.ru>
2021-10-31 06:55:21 +0300

    Diplom - 00

--- Diplom/src/main/Java/DiplomApplication.java
+++ Diplom/src/main/Java/DiplomApplication.java
@@ -1,3 +1,5 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class DiplomApplication {
 }
--- Diplom/src/main/Java/ru/otus/gpbu/gena/shell/MainShellCommands.java
+++ Diplom/src/main/Java/ru/otus/gpbu/gena/shell/MainShellCommands.java
@@ -1,5 +1,7 @@
 package ru.otus.gpbu.gena.shell;
 
+import org.springframework.shell.standard.ShellComponent;
+
 @ShellComponent
 public class MainShellCommands {
 }
