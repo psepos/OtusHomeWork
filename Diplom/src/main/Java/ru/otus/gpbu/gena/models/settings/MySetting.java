8812291efd16e1d12090809df351cbffdf1b8053
Palagin Sergey <palagos@yandex.ru>
2021-10-31 07:00:53 +0300

    Diplom - 00

---
+++ Diplom/src/main/Java/ru/otus/gpbu/gena/models/settings/MySetting.java
@@ -0,0 +1,27 @@
+package ru.otus.gpbu.gena.models.settings;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "author")
+@Getter
+@Setter
+public class MySetting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "value")
+    private String value;
+}
