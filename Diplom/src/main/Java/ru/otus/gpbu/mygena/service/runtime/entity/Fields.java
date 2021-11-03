44dde93fd9c33a9353520db6feaf738ceb7e9153
Palagin Sergey <sergey.palagin@gazprombank.ru>
2021-11-04 00:32:20 +0300

    Diplom

--- Diplom/src/main/Java/ru/otus/gpbu/mygena/service/runtime/entity/Fields.java
+++ Diplom/src/main/Java/ru/otus/gpbu/mygena/service/runtime/entity/Fields.java
@@ -4,7 +4,10 @@ import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.TypeSpec;
 import ru.otus.gpbu.mygena.models.myentity.MyEntity;
+import ru.otus.gpbu.mygena.models.myentityattribute.MyEntityAttribute;
 
+import javax.lang.model.element.Modifier;
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
@@ -25,9 +28,51 @@ public class Fields {
     public TypeSpec.Builder generate() {
 
         this.idField();
-
+        this.allField();
         return builder;
     }
+
+    private void allField() {
+        for (MyEntityAttribute attributeModel : entityModel.getAttributes()) {
+            this.field(attributeModel);
+        }
+    }
+
+    private void field(MyEntityAttribute attributeModel) {
+        switch (attributeModel.getType()) {
+            case ("Long"):
+                fieldLong(attributeModel);
+                break;
+            case ("String"):
+                fieldString(attributeModel);
+                break;
+        }
+    }
+
+    private void fieldString(MyEntityAttribute attributeModel) {
+        var ann = AnnotationSpec.builder(Column.class)
+                .addMember("name", "\"" + attributeModel.getCode() + "\"")
+                .build();
+
+        FieldSpec field = FieldSpec.builder(String.class, attributeModel.getCode())
+                .addModifiers(Modifier.PRIVATE)
+                .addAnnotation(ann)
+                .build();
+        builder.addField(field);
+    }
+
+    private void fieldLong(MyEntityAttribute attributeModel) {
+        var ann = AnnotationSpec.builder(Column.class)
+                .addMember("name", "\"" + attributeModel.getCode() + "\"")
+                .build();
+
+        FieldSpec field = FieldSpec.builder(Long.class, attributeModel.getCode())
+                .addModifiers(Modifier.PRIVATE)
+                .addAnnotation(ann)
+                .build();
+        builder.addField(field);
+    }
+
 
     private void idField() {
         var ann = AnnotationSpec.builder(GeneratedValue.class)
@@ -35,6 +80,7 @@ public class Fields {
                 .build();
 
         FieldSpec fieldId = FieldSpec.builder(Long.class, "id")
+                .addModifiers(Modifier.PRIVATE)
                 .addAnnotation(Id.class)
                 .addAnnotation(ann)
                 .build();
