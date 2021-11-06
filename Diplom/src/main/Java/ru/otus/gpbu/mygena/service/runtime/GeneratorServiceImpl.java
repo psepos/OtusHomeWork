34f78710b94a5a4639e133e67997e754301e161b
Palagin Sergey <palagos@yandex.ru>
2021-11-06 20:25:39 +0300

    diplom

--- Diplom/src/main/Java/ru/otus/gpbu/mygena/service/runtime/GeneratorServiceImpl.java
+++ Diplom/src/main/Java/ru/otus/gpbu/mygena/service/runtime/GeneratorServiceImpl.java
@@ -4,29 +4,17 @@ import com.squareup.javapoet.JavaFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.otus.gpbu.mygena.models.myentity.MyEntity;
-import ru.otus.gpbu.mygena.models.myentity.MyEntityService;
 import ru.otus.gpbu.mygena.models.mysetting.MySettingService;
-
-import java.io.IOException;
-import java.util.List;
 
 
 @Service
 public class GeneratorServiceImpl implements GeneratorService {
 
     @Autowired
-    private final MyEntityService myEntityService;
-
-    @Autowired
     private final MySettingService settings;
 
-    @Autowired
-    private final PathService pathService;
-
-    public GeneratorServiceImpl(MyEntityService myEntityService, MySettingService settings, PathService pathService) {
-        this.myEntityService = myEntityService;
+    public GeneratorServiceImpl(MySettingService settings) {
         this.settings = settings;
-        this.pathService = pathService;
     }
 
     @Override
