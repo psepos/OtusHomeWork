1acdecf7de07485472141d62d971a21cb02803f3
Palagin Sergey <palagos@yandex.ru>
2021-11-07 17:41:21 +0300

    diplom

--- Diplom/src/main/Java/ru/otus/gpbu/mygena/service/runtime/service/Methods.java
+++ Diplom/src/main/Java/ru/otus/gpbu/mygena/service/runtime/service/Methods.java
@@ -8,6 +8,7 @@ import ru.otus.gpbu.mygena.common.StringHelper;
 import ru.otus.gpbu.mygena.models.myentity.MyEntity;
 
 import javax.lang.model.element.Modifier;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -30,8 +31,60 @@ public class Methods {
 
         this.repositoryName = StringHelper.getStringFirstLower(entityModel.getCode() + "Repository");
 
+        this.findAllMethod();
         this.findByIdMethod();
+        this.deleteByIdMethod();
+        this.saveOrUpdateMethod();
         return builder;
+    }
+
+    private void saveOrUpdateMethod() {
+        String methodName = "saveOrUpdate";
+
+        String parameterName = StringHelper.getStringFirstLower(entityModel.getCode());
+        ClassName parameterType =  ClassName.get("", entityModel.getCode());
+
+        MethodSpec findByIdMethod = MethodSpec
+                .methodBuilder(methodName)
+                .addParameter(parameterType, parameterName)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(ClassName.get("", entityModel.getCode()))
+                .addCode(repositoryName + ".save(" + parameterName + ");\n")
+                .addCode("return " + parameterName + ";")
+                .build();
+
+        builder.addMethod(findByIdMethod);
+    }
+
+    private void deleteByIdMethod() {
+        String methodName = "deleteById";
+
+        String parameterName = "id";
+        ClassName parameterType =  ClassName.get(Long.class);
+
+        MethodSpec findByIdMethod = MethodSpec
+                .methodBuilder(methodName)
+                .addParameter(parameterType, parameterName)
+                .addModifiers(Modifier.PUBLIC)
+                .addCode(repositoryName + ".deleteById(id);")
+                .build();
+
+        builder.addMethod(findByIdMethod);
+    }
+
+    private void findAllMethod() {
+        String methodName = "findAll";
+
+        MethodSpec findByIdMethod = MethodSpec
+                .methodBuilder(methodName)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(ParameterizedTypeName.get(
+                        ClassName.get(List.class),
+                        ClassName.get("", entityModel.getCode())))
+                .addCode("return " + repositoryName + ".findAll();")
+                .build();
+
+        builder.addMethod(findByIdMethod);
     }
 
     private void findByIdMethod() {
