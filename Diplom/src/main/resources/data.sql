9c4b737789e02949b330479abb4c72dc954c0467
Palagin Sergey <palagos@yandex.ru>
2021-11-07 09:02:18 +0300

    diplom

--- Diplom/src/main/resources/data.sql
+++ Diplom/src/main/resources/data.sql
@@ -16,9 +16,9 @@ INSERT INTO setting (code, description, value) VALUES ('RUNTIME.ENVIRONMENT.COMP
 INSERT INTO setting (code, description, value) VALUES ('RUNTIME.ENVIRONMENT.COMPILE.MAVEN_SKIP_TESTS', '', 'YES');
 INSERT INTO setting (code, description, value) VALUES ('RUNTIME.ENVIRONMENT.COMPILE.MAVEN_BUILD_OPTIONS', '', 'package -T 1C -o -am');
 
-INSERT INTO entity (code, description) VALUES ('Entity1', 'EntityDescription1');
-INSERT INTO entity (code, description) VALUES ('Entity2', 'EntityDescription2');
-INSERT INTO entity (code, description) VALUES ('Entity3', 'EntityDescription3');
+INSERT INTO entity (code, description) VALUES ('Person', 'Person');
+INSERT INTO entity (code, description) VALUES ('Customer', 'Customer');
+INSERT INTO entity (code, description) VALUES ('Loan', 'Loan');
 INSERT INTO entity (code, description) VALUES ('Entity4', 'EntityDescription1');
 INSERT INTO entity (code, description) VALUES ('Entity5', 'EntityDescription2');
 INSERT INTO entity (code, description) VALUES ('Entity6', 'EntityDescription3');
@@ -87,13 +87,13 @@ INSERT INTO entity (code, description) VALUES ('Entity69', 'EntityDescription2')
 INSERT INTO entity (code, description) VALUES ('Entity70', 'EntityDescription3');
 
 
-INSERT INTO entity_attr (code, description, type, entity_id) VALUES ('Attribute1', 'AttributeDescription1', 'String', 1);
-INSERT INTO entity_attr (code, description, type, entity_id) VALUES ('Attribute2', 'AttributeDescription2', 'Long', 1);
-
+INSERT INTO entity_attr (code, description, type, entity_id) VALUES ('firstName', 'Firstname', 'String', 1);
+INSERT INTO entity_attr (code, description, type, entity_id) VALUES ('lastName', 'Lastname', 'String', 1);
+INSERT INTO entity_attr (code, description, type, entity_id) VALUES ('age', 'AttributeDescription2', 'Long', 1);
 
 INSERT INTO entity_attr (code, description, type, entity_id) VALUES ('Attribute3', 'AttributeDescription3', 'String', 2);
 INSERT INTO entity_attr (code, description, type, entity_id) VALUES ('Attribute4', 'AttributeDescription4', 'Long', 2);
 INSERT INTO entity_attr (code, description, type, entity_id) VALUES ('Attribute5', 'AttributeDescription4', 'String', 2);
 
-INSERT INTO entity_attr (code, description, type, entity_id) VALUES ('Attribute6', 'AttributeDescription6', 'String', 3);
-INSERT INTO entity_attr (code, description, type, entity_id) VALUES ('Attribute7', 'AttributeDescription7', 'Long', 3);
+INSERT INTO entity_attr (code, description, type, entity_id) VALUES ('code', 'code', 'String', 3);
+INSERT INTO entity_attr (code, description, type, entity_id) VALUES ('description', 'AttributeDescription7', 'String', 3);
