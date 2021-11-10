1aaf24071b5412663c6188a75dce99acfb0d46c0
Palagin Sergey <palagos@yandex.ru>
2021-11-10 08:46:45 +0300

    diplom

--- Diplom/src/main/Java/ru/otus/gpbu/mygena/models/myentity/controller/MyEntityRestController.java
+++ Diplom/src/main/Java/ru/otus/gpbu/mygena/models/myentity/controller/MyEntityRestController.java
@@ -2,9 +2,15 @@ package ru.otus.gpbu.mygena.models.myentity.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.otus.gpbu.mygena.models.myentity.dto.MyEntityDto;
 import ru.otus.gpbu.mygena.models.myentity.service.MyEntityService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/entities")
@@ -16,5 +22,18 @@ public class MyEntityRestController {
 
     public MyEntityRestController(MyEntityService myEntityService) {
         this.myEntityService = myEntityService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MyEntityDto>> findAllEntity() {
+
+        try{
+            List<MyEntityDto> all = myEntityService.findAll().stream().map(MyEntityDto::toDto).collect(Collectors.toList());
+            return ResponseEntity.ok().body(all);
+        }
+        catch (Exception e) {
+            log.error("findAllEntity: ",e);
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
--- Diplom/src/main/Java/ru/otus/gpbu/mygena/models/myentityattribute/dto/MyEntityAttributeDto.java
+++ Diplom/src/main/Java/ru/otus/gpbu/mygena/models/myentityattribute/dto/MyEntityAttributeDto.java
@@ -3,7 +3,6 @@ package ru.otus.gpbu.mygena.models.myentityattribute.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.otus.gpbu.mygena.models.myentity.dto.MyEntityDto;
 import ru.otus.gpbu.mygena.models.myentityattribute.MyEntityAttribute;
 
 import java.util.ArrayList;
@@ -11,21 +10,21 @@ import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
-public @Data class MyEntityAttributeDto {
+public @Data
+class MyEntityAttributeDto {
 
     private long id = -1;
     private String code;
     private String type;
-    private MyEntityDto entity;
 
-    public static MyEntityAttributeDto toDto(MyEntityAttribute attr){
-        return new MyEntityAttributeDto(attr.getId(), attr.getCode(), attr.getType(), MyEntityDto.toDto(attr.getEntity()));
+    public static MyEntityAttributeDto toDto(MyEntityAttribute attr) {
+        return new MyEntityAttributeDto(attr.getId(), attr.getCode(), attr.getType());
     }
 
-    public static List<MyEntityAttributeDto> toDto(List<MyEntityAttribute> attributes){
+    public static List<MyEntityAttributeDto> toDto(List<MyEntityAttribute> attributes) {
         List<MyEntityAttributeDto> listDto = new ArrayList<>();
 
-        for (MyEntityAttribute attrib : attributes){
+        for (MyEntityAttribute attrib : attributes) {
             listDto.add(MyEntityAttributeDto.toDto(attrib));
         }
         return listDto;
