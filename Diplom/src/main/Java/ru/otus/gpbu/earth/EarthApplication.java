de9633630bb0df915b89ef31198e10176c6e4f61
Palagin Sergey <palagos@yandex.ru>
2021-11-15 01:00:49 +0300

    123

--- Diplom/src/main/Java/ru/otus/gpbu/earth/EarthApplication.java
+++ Diplom/src/main/Java/ru/otus/gpbu/earth/EarthApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiplomApplication {
-
+public class EarthApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DiplomApplication.class, args);
+        SpringApplication.run(EarthApplication.class, args);
     }
 }
