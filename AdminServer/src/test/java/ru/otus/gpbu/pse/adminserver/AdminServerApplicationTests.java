ce3a1fb5d672ef21ad24c420cbf479dffe18191b
Palagin Sergey <palagos@yandex.ru>
2021-11-14 15:35:41 +0300

    123

---
+++ AdminServer/src/test/java/ru/otus/gpbu/pse/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.gpbu.pse.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
