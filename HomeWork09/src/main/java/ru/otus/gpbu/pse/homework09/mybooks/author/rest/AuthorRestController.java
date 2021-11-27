44b02185ed651b14678c521b2c39a10b3e8c03a7
Palagin Sergey <palagos@yandex.ru>
2021-11-28 00:28:10 +0300

    1

---
+++ HomeWork09/src/main/java/ru/otus/gpbu/pse/homework09/mybooks/author/rest/AuthorRestController.java
@@ -0,0 +1,9 @@
+package ru.otus.gpbu.pse.homework09.mybooks.author.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class AuthorRestController {
+
+
+}
